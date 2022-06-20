29c82955d3e5e25a7d8aef3565a0375bf5deffae
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-06-20 12:09:43 -0400

    Continuing setting up the project. Added Material UI and styled-components.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import {
   BrowserRouter as Router,
   Routes, Link, Route
 } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 import './index.css';
 import App from './App';
@@ -13,20 +14,26 @@ import NotFound from './components/not-found/not-found';
 import HomeView from './components/home-view/home-view';
 
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const theme = createTheme();
+
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<HomeView />} />
-          <Route path="data" element={<DataView />} />
-          <Route path="seating" element={<SeatView />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<HomeView />} />
+            <Route path="data" element={<DataView />} />
+            <Route path="seating" element={<SeatView />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
